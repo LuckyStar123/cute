ce7cdaca9db40edbf232ab0ece3ea22a6d7bf053
mm <1522308562@qq.com>
2015-06-21 10:08:35 +0800

    tab修改

--- web/demo3/js/default.js
+++ web/demo3/js/default.js
@@ -1,15 +1,15 @@
 var title=$("#title");
 title.click(function(ev){
-var ev=ev || window.event;
-var target=ev.target || ev.srcElement;
-if (target.nodeName.toLowerCase() == "div"){
-    $(".normal").css("background","url(images/title_bg.gif)");
-    $(target).css("background","url(images/title_bg2.gif)");
-    var id=target.id;
-    var i=id.substr(id.length-1,1);
-    var content_id="content_"+i;
-    $(".content1").css("display","none");
-    $("#"+content_id).css("display","block");             
+    var ev=ev || window.event;
+    var target=ev.target || ev.srcElement;
+    if (target.nodeName.toLowerCase() == "div"){
+        $(".normal").css("background","url(images/title_bg.gif)");
+        $(target).css("background","url(images/title_bg2.gif)");
+        var id=target.id;
+        var i=id.substr(id.length-1,1);
+        var content_id="content_"+i;
+        $(".content1").css("display","none");
+        $("#"+content_id).css("display","block");             
     }
 })  
             
